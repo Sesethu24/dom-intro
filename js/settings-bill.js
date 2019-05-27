3bde59299ee2a216303359d097624ed465e83045
Sesethu24 <seshmomoza20@gmail.com>
2019-05-27 14:31:53 +0200

    started another widget

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -20,3 +20,46 @@
 // * add nothing for invalid values that is not 'call' or 'sms'.
 // * display the latest total on the screen.
 // * check the value thresholds and display the total value in the right color.
+var callTotalSettingElem = document.querySelector(".callTotalTwo");
+var smsTotalSettingElem = document.querySelector(".smsTotalTwo");
+var checkedRadioBtnElem = document.querySelector(".radioBillAddBtn");
+var totalSetttingElem = document.querySelector(".totalTwo");
+var billItemTypeWithSettings = document.querySelector(".billItemTypeWithSettings");
+var settingTotalAddBtn = document.querySelector(".settingBillAddBtn");
+
+
+var callsTotalSet = 0;
+var smsTotalSet = 0;
+
+function radioBillTotal(){
+     
+    
+if (checkedRadioBtnElem){
+    var billItemTypeRadio = document.querySelector("input[name='billItemType']:checked");
+
+}
+    if (billItemTypeRadio.value === "call"){
+        callsTotalSet += 2.75
+    }
+    else if (billItemTypeRadio.value === "sms"){
+        smsTotalSet += 0.75;
+    }
+      
+  
+    callsTotalSettingsElem.innerHTML = callsTotalSet.toFixed(2);
+    smsTotalSettingElem.innerHTML = smsTotalSet.toFixed(2);
+    var totalSettingCost = callsTotalSet + smsTotalSet;
+    totalSettingElem.innerHTML = totalSettingCost.toFixed(2); 
+
+    
+    if (totalSettingCost >= 50){
+        
+        totalSettingElem.classList.add("danger");
+    }
+    else if (totalSettingCost >= 30){
+        totalSettingElem.classList.add("warning");
+    }
+
+}
+ checkedRadioBtnElem.addEventListener('click', radioBillTotal);
+
