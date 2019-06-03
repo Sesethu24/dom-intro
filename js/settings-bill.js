2da8815960ad916b5e7c82d8e2394d2046024222
Sesethu24 <seshmomoza20@gmail.com>
2019-06-03 12:11:25 +0200

    finished my widgets

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -5,12 +5,23 @@ var costCalls = document.querySelector(".callTotalSettings");
 var costSms = document.querySelector(".smsTotalSettings");
 var costSettings = document.querySelector(".totalSettings");
 
+var callsElement = document.querySelector(".callCostSetting");
+var smsElement = document.querySelector(".smsCostSetting");
+var warningLevel = document.querySelector(".warningLevelSetting");
+var criticalLevel = document.querySelector(".criticalLevelSetting");
+var updateSettingsBtn = document.querySelector(".updateSettings");
 
+var callElement = 0;
+var SmssElement = 0;
+var warningLev = 0;
+var criticalLev = 0;
 
 var Calls = 0;
 var Sms = 0;
+var totalCost = 0;
 
 function AddFunction(){
+    if(!(totalCost >= criticalLev)){
      
     settingsButton = document.querySelector("input[name='radioButton']:checked");
     if (settingsButton){
@@ -19,32 +30,64 @@ function AddFunction(){
     }
    
     if (radioButton === "call"){
-        Calls += 2.75
+        Calls += callElement;
     }
     else if (radioButton === "sms"){
-        Sms += 0.75;
+        Sms += SmssElement;
     }
     
   
     costCalls.innerHTML = Calls.toFixed(2);
     costSms.innerHTML = Sms.toFixed(2);
-    var totalCost = Calls + Sms;
+     totalCost = Calls + Sms;
     costSettings.innerHTML = totalCost.toFixed(2); 
 
     
-    //if (totalCost >= 50){
+    if (totalCost >= criticalLev){
         
-      //  totalCostTwoElem.classList.add("danger");
-   // }
-   // else if (totalCost >= 30){
-       // totalCostTwoElem.classList.add("warning");
-   // }
-
+        costSettings.classList.add("danger");
+        costSettings.classList.remove("warning");
+    }
+    else if (totalCost >= warningLev){
+        costSettings.classList.add("warning");
+        costSettings.classList.remove("danger");
+    }
+}
 }
  settingsButton.addEventListener('click', AddFunction);
     
  function UpdateButton()
  {
+    callElement = Number(callsElement.value);
+    SmssElement = Number(smsElement.value);
+    warningLev = Number(warningLevel.value);
+    criticalLev = Number(criticalLevel.value);
 
+
+     
+
+    // if (totalCost < criticalLev){
+    //     settingsButton.disabled = false;
+    // }
+    // if (totalCost >= criticalLev){
+    //     settingsButton.disabled = true;
+    // }
+    if (totalCost >= criticalLev){
+        
+        costSettings.classList.add("danger");
+        costSettings.classList.remove("warning");
+    }
+    else if (totalCost >= warningLev){
+        costSettings.classList.add("warning");
+        costSettings.classList.remove("danger");
+    }
+    if (totalCost < warningLev){
+        costSettings.classList.remove("warning");
+        costSettings.classList.remove("danger");
+    }
+    if (criticalLev < totalCost ){
+        costSettings.classList.remove("warning");
+        costSettings.classList.remove("danger");
+    }
  }
-    
+ updateSettingsBtn.addEventListener('click', UpdateButton);
